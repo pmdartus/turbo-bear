04496cc401c734ed62b07b960de82a28082edc8a
Pierre-Marie Dartus <p.dartus@salesforce.com>
2021-11-05 07:23:56 +0100

    Run tests on different threads

--- tests/fixtures.rs
+++ tests/fixtures.rs
@@ -1,12 +1,11 @@
 use std::fs;
 
+use test_generator::test_resources;
 use turbo_bear::parser::parse_program;
 
-#[test]
-fn parse_fixture_tests() {
-    insta::glob!("fixtures/*.tb", |path| {
-        let input = fs::read_to_string(path).unwrap();
-        let program = parse_program(&input);
-        insta::assert_debug_snapshot!(program);
-    });
+#[test_resources("tests/fixtures/*.tb")]
+fn fixture(path: &str) {
+    let input = fs::read_to_string(path).unwrap();
+    let program = parse_program(&input);
+    insta::assert_debug_snapshot!(program);
 }
